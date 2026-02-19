6561c633006367fc93e8256a274f9dbb37f76d7a
lindsayspoor <lindsayspoor@gmail.com>
2026-02-19 10:22:22 +0100

    added formula1 env to env list

--- omnisafe/envs/safety_gymnasium_env.py
+++ omnisafe/envs/safety_gymnasium_env.py
@@ -121,6 +121,7 @@ class SafetyGymnasiumEnv(CMDP):
         'SafetyHumanoidVelocity-v1',
         'SafetyPointRun0-v0',
         'SafetyCarRun0-v0',
+        'SafetyPointFormulaOne1-v0',
     ]
 
     def __init__(
